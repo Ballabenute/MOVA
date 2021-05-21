ad120d717afab52e29a048709259049b8b189a5c
Ballabenute <vitor.ballabenute@gmail.com>
2021-05-20 23:15:55 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,9 @@
 @section('content')
 
     <div style="display: inline-flex; margin-top: 100px;">
-        <img src="{{ URL::asset('/image/html5.png') }}" alt="icon" style="height:200px;margin-right: 20px;">
-        <img src="{{ URL::asset('/image/php.png') }}" alt="icon" style="height:200px">
+        <img src="{{ URL::asset('/image/php.png') }}" alt="icon" style="height:200px; margin-left: 10%;">
         <img src="{{ URL::asset('/image/laravel.png') }}" alt="icon" style="height:200px">
         <img src="{{ URL::asset('/image/mysql.png') }}" alt="icon" style="height:200px">
-
     </div>
 
 @stop
